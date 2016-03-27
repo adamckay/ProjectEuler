4d7f85399e3d5905a28e718c860e4ccbe561a69c
Adam McKay <adammckay92@googlemail.com>
2016-03-27 18:45:03 +0100

    Added answer string to 1

--- 001.py
+++ 001.py
@@ -8,4 +8,4 @@ Find the sum of all the multiples of 3 or 5 below 1000.
 ANSWER: 233168
 """
 
-print sum([x for x in xrange(1000) if x % 3 == 0 or x % 5 == 0])
+print "ANSWER: " + str(sum([x for x in xrange(1000) if x % 3 == 0 or x % 5 == 0]))
